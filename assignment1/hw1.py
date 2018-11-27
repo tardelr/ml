3dfa599316e256bbfaa1f2ef04b5564b192c29ee
Renan Tardelli <renantardelli@Renans-MacBook-Pro.local>
2018-11-26 23:46:33 -0200

    correcoes na vmv_product

--- assignment1/hw1.py
+++ assignment1/hw1.py
@@ -18,38 +18,14 @@ np.savetxt("wRR_" + str(lambda_input) + ".csv", wRR, delimiter="\n") # write out
 
 
 ## Solution for Part 2
-def trick_to_avoid_sum(matrix):
-	"""
-	Avoid sum of inner products like Sum(xi * xi.T)
-	"""
-	d,_ = martix.shape
-	return sum(matrix * matrix.T * np.eye(d))
-
 def covariance_matrix(X,y,lmbda,sig):
 	n,d = X.shape
 	return np.linalg.inv((lmbda * np.eye(d)) + (sig ** -2) * X * X.T)
 
-def mu_map(lmbda, sigma2, X, y):
-	n,d = X.shape
-	return np.linalg.inv(lmbda * sigma2 * np.eye(d) + X.T*X) * np.dot(X.T, y)
 
-def covariance_posterior(X, x0, lmbda, sigma2_input):
-	n,d = X.shape
-	inner_part= (x0 * x0.T + trick_to_avoid_sum(X))
-	return 	np.linalg.inv(lmbda * np.eye(d) + (sigma2_input ** -1) * inner_part)
-
-def mu_posterior(X, y, x0, y0, lmbda, sigma2_input):
-	inner_part_first = (x0 * x0.T + trick_to_avoid_sum(X))
-	first_term = np.linalg.inv(lmbda * sigma2_input * np.eye(d) + inner_part_first)
-	second_term = np.dot(x0, y0) + sum(X * y.T * np.eye(d))
-	return 	first_term + second_term
 
 def part2():
     pass
 
 active = part2()  # Assuming active is returned from the function
 np.savetxt("active_" + str(lambda_input) + "_" + str(int(sigma2_input)) + ".csv", active, delimiter=",") # write output to file
-
-
-
-
